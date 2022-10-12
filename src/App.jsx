5888d5f5b07f087b551d841cdc898fc76dabbece
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-10-11 23:42:05 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import Logo from "./images/bp_logo_512.png";
 import "./myCss.css";
 import OnePost from "./OnePost";
 import AllPosts from "./AllPosts";
+import gmnabi from "./gmnabi.json";
 
 const { ethers } = require("ethers");
 /*
@@ -333,281 +334,8 @@ function App(props) {
     try {
       // validate token balance
       const tokenAddress = "0xfD18418c4AEf8edcAfF3EFea4A4bE2cC1cF2E580";
-      const abi = [
-        {
-          inputs: [
-            { internalType: "string", name: "_name", type: "string" },
-            { internalType: "string", name: "_symbol", type: "string" },
-            { internalType: "string", name: "_initBaseURI", type: "string" },
-          ],
-          stateMutability: "nonpayable",
-          type: "constructor",
-        },
-        {
-          anonymous: false,
-          inputs: [
-            { indexed: true, internalType: "address", name: "owner", type: "address" },
-            { indexed: true, internalType: "address", name: "approved", type: "address" },
-            { indexed: true, internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "Approval",
-          type: "event",
-        },
-        {
-          anonymous: false,
-          inputs: [
-            { indexed: true, internalType: "address", name: "owner", type: "address" },
-            { indexed: true, internalType: "address", name: "operator", type: "address" },
-            { indexed: false, internalType: "bool", name: "approved", type: "bool" },
-          ],
-          name: "ApprovalForAll",
-          type: "event",
-        },
-        {
-          anonymous: false,
-          inputs: [
-            { indexed: true, internalType: "address", name: "previousOwner", type: "address" },
-            { indexed: true, internalType: "address", name: "newOwner", type: "address" },
-          ],
-          name: "OwnershipTransferred",
-          type: "event",
-        },
-        {
-          anonymous: false,
-          inputs: [
-            { indexed: true, internalType: "address", name: "from", type: "address" },
-            { indexed: true, internalType: "address", name: "to", type: "address" },
-            { indexed: true, internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "Transfer",
-          type: "event",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "to", type: "address" },
-            { internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "approve",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "address", name: "owner", type: "address" }],
-          name: "balanceOf",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "cost",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
-          name: "getApproved",
-          outputs: [{ internalType: "address", name: "", type: "address" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "owner", type: "address" },
-            { internalType: "address", name: "operator", type: "address" },
-          ],
-          name: "isApprovedForAll",
-          outputs: [{ internalType: "bool", name: "", type: "bool" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "maxMintAmount",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "maxSupply",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "_mintAmount", type: "uint256" }],
-          name: "mint",
-          outputs: [],
-          stateMutability: "payable",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "name",
-          outputs: [{ internalType: "string", name: "", type: "string" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "owner",
-          outputs: [{ internalType: "address", name: "", type: "address" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
-          name: "ownerOf",
-          outputs: [{ internalType: "address", name: "", type: "address" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "bool", name: "_state", type: "bool" }],
-          name: "pause",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "paused",
-          outputs: [{ internalType: "bool", name: "", type: "bool" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        { inputs: [], name: "renounceOwnership", outputs: [], stateMutability: "nonpayable", type: "function" },
-        {
-          inputs: [
-            { internalType: "address", name: "from", type: "address" },
-            { internalType: "address", name: "to", type: "address" },
-            { internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "safeTransferFrom",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "from", type: "address" },
-            { internalType: "address", name: "to", type: "address" },
-            { internalType: "uint256", name: "tokenId", type: "uint256" },
-            { internalType: "bytes", name: "_data", type: "bytes" },
-          ],
-          name: "safeTransferFrom",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "operator", type: "address" },
-            { internalType: "bool", name: "approved", type: "bool" },
-          ],
-          name: "setApprovalForAll",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "string", name: "_newBaseURI", type: "string" }],
-          name: "setBaseURI",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "_newCost", type: "uint256" }],
-          name: "setCost",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "_newmaxMintAmount", type: "uint256" }],
-          name: "setmaxMintAmount",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
-          name: "supportsInterface",
-          outputs: [{ internalType: "bool", name: "", type: "bool" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "symbol",
-          outputs: [{ internalType: "string", name: "", type: "string" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "index", type: "uint256" }],
-          name: "tokenByIndex",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "owner", type: "address" },
-            { internalType: "uint256", name: "index", type: "uint256" },
-          ],
-          name: "tokenOfOwnerByIndex",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
-          name: "tokenURI",
-          outputs: [{ internalType: "string", name: "", type: "string" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [],
-          name: "totalSupply",
-          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "from", type: "address" },
-            { internalType: "address", name: "to", type: "address" },
-            { internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "transferFrom",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
-          name: "transferOwnership",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [{ internalType: "address", name: "_owner", type: "address" }],
-          name: "walletOfOwner",
-          outputs: [{ internalType: "uint256[]", name: "", type: "uint256[]" }],
-          stateMutability: "view",
-          type: "function",
-        },
-        { inputs: [], name: "withdraw", outputs: [], stateMutability: "payable", type: "function" },
-      ];
 
-      const tokenContract = new ethers.Contract(tokenAddress, abi, userSigner);
+      const tokenContract = new ethers.Contract(tokenAddress, gmnabi, userSigner);
 
       const balance = await tokenContract.balanceOf(address);
 
@@ -651,7 +379,36 @@ function App(props) {
         </div>
       </Header>
 
-      <Button
+      <button   className="mint"
+                onClick={async () => {
+                  /* look how you call setPurpose on your contract: */
+                  /* notice how you pass a call back for tx updates too */
+                const contract = new ethers.Contract("0xfD18418c4AEf8edcAfF3EFea4A4bE2cC1cF2E580", gmnabi, userSigner);
+
+                  const cost = contract.cost(); 
+                  const result = tx(contract.mint(1, {value: cost}), update => {
+                    console.log("📡 Transaction Update:", update);
+                    if (update && (update.status === "confirmed" || update.status === 1)) {
+                      
+                      sendNotification("success", {
+                        message: "Minted",
+                        description: `You can now view any article of your choice.`,
+                      });
+                      console.log(" 🍾 Transaction " + update.hash + " finished!");
+                      console.log(
+                        " ⛽️ " +
+                          update.gasUsed +
+                          "/" +
+                          (update.gasLimit || update.gas) +
+                          " @ " +
+                          parseFloat(update.gasPrice) / 1000000000 +
+                          " gwei",
+                      );
+                    }
+                  });
+                  console.log("awaiting metamask/web3 confirm result...", result);
+                  console.log(await result);
+                }}
         style={{
           position: "fixed",
           bottom: "10px",
@@ -663,8 +420,8 @@ function App(props) {
         }}
         type="default"
       >
-        Buy Paper
-      </Button>
+        Mint
+      </button>
 
       <div className="min-h-screen p-12">
         <div className="container mx-auto">
